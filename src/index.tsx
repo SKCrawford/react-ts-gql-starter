00b04be41177f80c04a551a9c92972768737d9be
SKCrawford <s.kyle.crawford@gmail.com>
2019-06-17 11:43:54 -0700

    Add graphql, routing, linting, fixing, and storybook

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import * as React from 'react';
+import { render } from 'react-dom';
+
+import { App } from './app';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+if (module.hot) {
+  module.hot.accept();
+}
+
+render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
